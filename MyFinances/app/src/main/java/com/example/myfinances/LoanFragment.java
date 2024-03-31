4e2f6a6456e3cc1eb11581c7c00bdf76e72377c9
Vigneswari Sambandam <vigneshwarie@gmail.com>
2024-03-31 16:47:24 -0400

    Added loan form, included functionality on main, added headings

---
+++ MyFinances/app/src/main/java/com/example/myfinances/LoanFragment.java
@@ -0,0 +1,11 @@
+package com.example.myfinances;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoanFragment extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_loans);
+    }
+}
